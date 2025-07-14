93147fed0c508a4b227ec881219795046182b49a
Mateo <mateolacerenza1@gmail.com>
2025-07-14 10:43:11 -0300

    Completacion de metodos y operaciones

--- Operations.cs
+++ Operations.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 public class Operations
 {
     public static double Sum(double a, double b)
@@ -5,21 +7,21 @@ public class Operations
         return a + b;
     }
 
-    public static double Restar(double a, double b)
+    public static double Subtract(double a, double b)
     {
         return a - b;
     }
 
-    public static double Mult(double a, double b)
+    public static double Multiplication(double a, double b)
     {
         return a * b;
     }
 
     public static double Division(double a, double b)
     {
-        if (b == 0)
+        if (b == 0 || a == 0)
         {
-            Console.WriteLine("No se puede dividir por cero.");
+            Console.WriteLine("You cant divide 0");
             return double.NaN;
         }
         return a / b;
--- obj/Debug/net9.0/CalculadoraCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CalculadoraCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4183c9e023e5558935e800261540b49cec9c42bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e66248383da309ae62844cd0b3c6d7fa9708f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
